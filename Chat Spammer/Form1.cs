af1bb1a174d979cd2c5b8ccd41040ed85d7ba9a0
roast247 <77567356+roast247@users.noreply.github.com>
2021-02-10 20:50:31 -0800

    Add files via upload

---
+++ Chat Spammer/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chat_Spammer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            SendKeys.Send(RichTextBox1.Text);
+            SendKeys.Send("{ENTER}");
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void kryptonButton1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { }
+        private bool _dragging = false;
+        private Point _offset;
+        private Point _start_point = new Point(0, 0);
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            _dragging = true;
+            _start_point = new Point(e.X, e.Y);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_dragging)
+            {
+                Point p = PointToScreen(e.Location);
+                Location = new Point(p.X - this._start_point.X, p.Y - this._start_point.Y);
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            _dragging = false;
+        }
+    }
+}
